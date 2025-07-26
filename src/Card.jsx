867f3419faa5c496a890ac48e243fb100981e29c
Sanithu Malhiru <119713517+SanithuM@users.noreply.github.com>
2025-07-26 15:47:48 +0530

    Add files via upload

---
+++ src/Card.jsx
@@ -0,0 +1,13 @@
+
+function Card(){
+    return(
+        <div className="card">
+            <img className="card-image" src="https://preview.redd.it/of-all-the-hokage-who-is-the-worst-person-morally-v0-tgtefhh0x7yb1.jpg?width=640&crop=smart&auto=webp&s=72f8d7ce19499c7103b018247dee10ba26529476" alt="profile picture"></img>
+            <h2 className="card-title">Tobirama Senju</h2>
+            <p className="card-text">Make Konoha great again</p>
+        </div>
+    
+    );
+}
+
+export default Card
